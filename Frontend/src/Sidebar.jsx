ef9b5393e587c86c28d868156120f5c2c2307740
Sanjay067 <sanjaygowda00006@gmail.com>
2025-12-12 10:31:50 +0530

    Added toogle function for sidebar

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -8,12 +8,14 @@ import './Sidebar.css';
 // }
 
 import { MyContext } from './MyContext';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 function Sidebar() {
 
-    const { allThreads, setAllThreads, reply, setReply, prevChats, latestReply, setLatestReply, setPrevChats, currThread, setCurrThread, newChat, setNewChat, loader ,setLoader } = useContext(MyContext);
+    const [isOpen, setIsOpen] = useState(true);
+
+    const { allThreads, setAllThreads, reply, setReply, prevChats, latestReply, setLatestReply, setPrevChats, currThread, setCurrThread, newChat, setNewChat, loader, setLoader } = useContext(MyContext);
 
     const getAllThreads = async () => {
         try {
@@ -77,46 +79,53 @@ function Sidebar() {
 
     }
 
+    const toggleSidebar = () => {
+        setIsOpen(prev => !prev);
+    }
+
     return (
-        <div className="sidebar">
-            <div className="sidebar__top">
-                <img className="sidebar__logo" src="src/assets/ChatGPT-Logo.svg.png" alt="logo" />
-                <button className="sidebar__close_btn">
-                    <span className="sidebar__close_icon">
-                        <i className="fa-solid fa-xmark"></i>
+        <>
+            <div className="display_sidebar" style={{ display: isOpen ? "none" : "block" }}><button onClick={toggleSidebar}><i className="fa-solid fa-list"></i></button></div>
+            <div className="sidebar" style={{ display: isOpen ? "block" : "none" }}>
+
+                <div className="sidebar__top">
+                    <img className="sidebar__logo" src="src/assets/ChatGPT-Logo.svg.png" alt="logo" />
+                    <button className="sidebar__close_btn" onClick={toggleSidebar}>
+                        <span className="sidebar__close_icon">
+                            <i className="fa-solid fa-xmark"></i>
+                        </span>
+                    </button>
+                </div>
+                <div className="sidebar__new-chat">
+                    <button className="sidebar__new_chat_btn" disabled={loader} onClick={() => createChat()}>
+                        <i className="fa-solid fa-pen-to-square sidebar__new-chat-icon"></i>
+                        New Chat
+                    </button>
+                </div>
+
+                <div className="sidebar__history">
+                    <h3 className="sidebar__history-title">Chats</h3>
+                    <ul className="sidebar__threads">
+                        {
+                            allThreads?.map((thread, idx) => (
+                                <li key={idx} onClick={() => getThread(thread.threadId)} >{thread.title}
+                                    <button onClick={() => {
+                                        deleteThread(thread.threadId)
+                                    }}><i className="fa-solid fa-trash"></i></button></li>
+                            )
+                            )
+                        }
+                    </ul>
+                </div>
+
+                <div className="sidebar__bottom">
+                    <span className="sidebar__user-icon-wrapper">
+                        <i className="fa-solid fa-user"></i>
                     </span>
-                </button>
+                    <h3 className="sidebar__username">Sanjay</h3>
+                </div>
             </div>
-
-            <div className="sidebar__new-chat">
-                <button className="sidebar__new_chat_btn" disabled = {loader}  onClick={() => createChat()}>
-                    <i className="fa-solid fa-pen-to-square sidebar__new-chat-icon"></i>
-                    New Chat
-                </button>
-            </div>
-
-            <div className="sidebar__history">
-                <h3 className="sidebar__history-title">Chats</h3>
-                <ul className="sidebar__threads">
-                    {
-                        allThreads?.map((thread, idx) => (
-                            <li key={idx} onClick={() => getThread(thread.threadId)} >{thread.title}
-                                <button onClick={() => {
-                                    deleteThread(thread.threadId)
-                                }}><i className="fa-solid fa-trash"></i></button></li>
-                        )
-                        )
-                    }
-                </ul>
-            </div>
-
-            <div className="sidebar__bottom">
-                <span className="sidebar__user-icon-wrapper">
-                    <i className="fa-solid fa-user"></i>
-                </span>
-                <h3 className="sidebar__username">Sanjay</h3>
-            </div>
-        </div>
+        </>
     );
 }
 
